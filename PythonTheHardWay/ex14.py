32888b8cae5981e6bb204cd01c481db4ba4f89f2
Shraddha-kr <shraddhakr@outlook.com>
2022-02-04 10:26:54 +0530

    Adding in everyday

--- PythonTheHardWay/ex14.py
+++ PythonTheHardWay/ex14.py
@@ -5,7 +5,7 @@ prompt = '> '
 
 print(f"Hi {user_name}, I'm the {script} script.")
 print("I'd like to ask you a few questions.")
-print("Do you like me {user_name}?")
+print(f"Do you like me {user_name}?")
 likes = input(prompt)
 
 print(f"Where do you live {user_name}?")
